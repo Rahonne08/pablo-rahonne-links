c56177d63115606f2062db696e5e4c0e2605da42
Pablo Rahonne <pablorahonne.pr@gmail.com>
2024-08-15 18:51:32 -0300

    adicionado itens claros e escuros

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 document.getElementById('toggleButton').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
     
-    const buttonText = document.body.classList.contains('dark-mode') ? 'Modo Claro' : 'Modo Escuro';
+    const buttonText = document.body.classList.contains('dark-mode') ? '🌕' : '🌑';
     this.textContent = buttonText;
 });
